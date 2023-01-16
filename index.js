503d4154c46391a13d2fd12cd7a4b6900e0d36b6
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-01-16 12:45:58 -0600

    completed labwork

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(array) {
+    let foundWin = array.find(isWin)
+    if (foundWin === undefined) {
+        return undefined
+    }
+    return foundWin.year
+}
+
+function isWin(element) {
+    return element.result === "W"
+}
